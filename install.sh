46e418446ed3213cc8f122b870867ed100a709bf
metelkinas <metelkin.as@gmail.com>
2016-12-07 22:12:38 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 GetOSName () {
 if [ -n "$(cat /etc/*-release | grep Ubuntu)" ]
    then
@@ -20,13 +21,17 @@ return 1
 }
 
 GetOSVersion () {
-if [ "$OSName" != "Red Hat Enterprise" ]
+if [ "$OSName" = "CentOS" ]
    then
-      temp=$(cat /etc/*-release | grep VERSION_ID)
-      echo $temp	
+      OSVersion=$((cat /etc/*-release | grep VERSION_ID) | sed -e 's/.*"\(.*\)".*/\1/')
+      return 0
+   else
+      OSVersion=$(lsb_release -rs)
+      return 0
 fi
 }
 
 GetOSName
 echo $OSName
 GetOSVersion
+echo $OSVersion
