1eae6fd5697fd2621ff06874de7bda01289cacbd
alex <alex@ubuntusrv.petersburg.epam.com>
2016-12-08 00:59:37 +1000

    q1

--- install.sh
+++ install.sh
@@ -12,29 +12,24 @@ if [ -n "$(cat /etc/*-release | grep CentOS)" ]
 fi	
 if [ -n "$(cat /etc/*-release | grep "Red Hat Enterprise")" ]
    then
-      OSName="Red Hat Enterprise"
+      OSName="RHEL"
       return 0		
 fi
-OSName="XZ"
 return 1
 }
 
 GetOSVersion () {
-#if [ "$OSName" = "CentOS" ]
-#   then
-#      OSVersion=$((cat /etc/*-release | grep VERSION_ID) | sed -e 's/.*"\(.*\)".*/\1/')
-#      return 0
-#   else
-#      OSVersion=$(lsb_release -rs)
-#      return 0
-#fi
-OSVersion=$(lsb_release -rs)
-if [ -z "$OSVersion" ]
+if [ "$OSName" = "Ubuntu" ]
    then
-      OSVersion=$((cat /etc/*-release | grep VERSION_ID) | sed -e 's/.*"\(.*\)".*/\1/')
+      OSVersion=$(lsb_release -rs)
+      return 0	
+fi
+if [ "$OSName" = "RHEL" ]
+   then
+      OSVersion=$(cat /etc/*-release | grep -m 1 release | awk '{ print $7 }')
       return 0
-fi	
-return 0
+fi
+#cat /etc/*-release | grep -m 1 release
 }
 
 GetOSName
