b0610a1fba8f0a479b96def29ae4381b409819b7
alex <alex@ubuntusrv.petersburg.epam.com>
2016-12-08 22:14:24 +1000

    install tomcat

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@ GetOSName () {
 if [ -n "$(cat /etc/*-release | grep Ubuntu)" ]
    then
       OSName="Ubuntu"
-      return 1	
+      return 0	
 fi
 if [ -n "$(cat /etc/*-release | grep CentOS)" ]
    then
       OSName="CentOS"
-      return 1
+      return 0
 fi	
 if [ -n "$(cat /etc/*-release | grep "Red Hat Enterprise")" ]
    then
       OSName="RHEL"
-      return 1
+      return 0
 fi
 return 1
 }
@@ -34,7 +34,7 @@ OSVersion=${temp//[^0-9.]}
 }
 
 CheckInstallJava () {
-if type -p java 
+if type -p java &> /dev/null
 then
    return 0
 else
@@ -42,13 +42,54 @@ else
 fi   
 }
 
+GetVersionJava () {
+JavaVer=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+}
 
-GetOSName
-res=$?
-if [ $res -ne 0 ]
+CheckInstallTomcat () {
+#temp=$(ps -ef | grep -m 1 tomcat | awk '{print $1}')
+#echo $temp
+#echo $USER
+if [[ "$(ps -ef | grep -m 1 tomcat | awk '{print $1}')" != "$USER" ]]
+   then 
+      return 0
+fi
+if [ "$OSName" = "Ubuntu" ]
    then
+      if [ -z "$(dpkg -l | grep tomcat)" ]
+         then
+            return 1 
+         else
+            return 0   
+      fi
+fi
+if [ -z "$(yum list installed | grep tomcat)" ]
+   then
+      return 1
+   else
+      return 0
+fi
+}
+
+#CheckVersionTomcat () {
+#
+#}
+
+IsRoot () {
+if [ "$(id -u)" = "0" ]; 
+   then
+      return 0
+   else
+      return 1
+fi
+}
+
+if GetOSName
+   then
+      :
+   else
       echo "Ошибка. Неподдерживаемый тип ОС."
-      exit 1	
+      exit 1     
 fi
 
 GetOSVersion
@@ -76,12 +117,31 @@ case $OSName in
    ;;
 esac
 
-#CheckInstallJava
 if CheckInstallJava
-   then
-      echo "Java installed"
+   then      
+      GetVersionJava
+      if [[ "$JavaVer" < "1.8" ]]
+         then
+            echo "Необходимо установить Java выше 1.8"
+            exit 1
+      fi   
    else
-      echo "Java not installed"
+      NeedJava=true
 fi
 
+if CheckInstallTomcat
+   then
+      echo "Tomcat installed"
+   else
+      echo "Tomcat not installed"
+fi
+#CheckVersionTomcat
+
+#if IsRoot
+#   then
+#   :
+#else
+#   echo "Все проверки пройдены, но для установки скрипт должен быть запущен от root"
+#   exit 1
+#fi
 
